c6001d0267f9b360bff9d7fcc7c8229f891b30f2
Matheus Fernando da Silva <149023228+MattFernando@users.noreply.github.com>
2026-06-12 16:52:56 -0300

    bottom navigation bar arrumado

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ifood_aplication/pagamento.dart';
 
 class checkout extends StatefulWidget {
   const checkout({super.key});
@@ -11,7 +12,6 @@ class _checkoutState extends State<checkout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text("Sacola"),
@@ -34,6 +34,76 @@ class _checkoutState extends State<checkout> {
           ),
         ],
       ),
+      //Tinha feito o botao por fora do bottom navigator bar, entao pode ter coisa ou outra fora, verificar mais tarde
+      bottomNavigationBar: 
+      Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children:[
+                        Text(
+                          'Total com a entrega',
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                        SizedBox(height: 2),
+                        Row(
+                          children: [
+                            Text(
+                              'R\$ 59,04',
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              '/ 1 item',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 170,
+                    height: 52,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Pagamento()),
+                        );},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFE41E2B),
+                        foregroundColor: Colors.white,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        'Continuar',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
@@ -82,9 +152,7 @@ class _checkoutState extends State<checkout> {
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
                   ),
 
-
                   SizedBox(height: 12),
-
 
                   Row(
                     children: [
@@ -92,13 +160,12 @@ class _checkoutState extends State<checkout> {
 
                       SizedBox(width: 8),
 
-
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             "Av. Brasil, 99",
-                            style: TextStyle(fontWeight: FontWeight.w500),
+                            style: TextStyle(fontWeight: FontWeight.w600),
                           ),
                           Text(
                             "São Roque",
@@ -107,17 +174,223 @@ class _checkoutState extends State<checkout> {
                         ],
                       ),
                       Spacer(),
-                      TextButton(onPressed: null, child: Text("Trocar", style: TextStyle(color: Colors.red, fontWeight: FontWeight.w600),)),
+                      TextButton(
+                        onPressed: null,
+                        child: Text(
+                          "Trocar",
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
                     ],
+                  ),
+
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Text(
+                        "Opções de entrega",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 8),
+                        width: 20,
+                        height: 20,
+                        decoration: const BoxDecoration(
+                          color: Colors.black,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          size: 13,
+                          Icons.question_mark_rounded,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+
+                  Container(
+                    padding: EdgeInsets.all(25),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 230, 230, 230),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 133, 133, 133),
+                        width: 1,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(width: 8),
+
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Rápida",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 136, 136, 136),
+                                fontSize: 15,
+                              ),
+                            ),
+                            Text(
+                              "Hoje, 38 - 53 min",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 136, 136, 136),
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          children: [
+                            Text(
+                              "R\$ 4,99",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 136, 136, 136),
+                                fontSize: 13,
+                              ),
+                            ),
+                            Text(
+                              "+ R\$ 3,00",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 136, 136, 136),
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.all(25),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 230, 230, 230),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        width: 1,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(width: 8),
+
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Agendada",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                Text(
+                                  "Hoje, 11:00 - 11:30",
+                                  style: TextStyle(
+                                    color: const Color.fromARGB(
+                                      255,
+                                      136,
+                                      136,
+                                      136,
+                                    ),
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                            Container(
+                              margin: EdgeInsets.only(top: 10),
+                              child:
+                            Row(
+                              children: [
+                                Text(
+                                  "R\$ 4,99",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(left: 5),
+                                  width: 32,
+                                  height: 32,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.red,
+                                  ),
+                                  child: Center(
+                                    child: Container(
+                                      width: 20,
+                                      height: 20,
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Trocar de Horário",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.red,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Taxa grátis ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(" retirando o sei pedido na loja >"),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-
-            
-
-
-
+            SizedBox(height: 100),
           ],
         ),
       ),
